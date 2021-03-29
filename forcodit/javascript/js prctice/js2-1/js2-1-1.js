5099f1b7d84848af31db5000b94b3ba980acd56a
HJK9810 <ruby45342@gamail.com>
2021-03-29 22:49:43 +0900

    0329 codeit study

---
+++ forcodit/javascript/js prctice/js2-1/js2-1-1.js
@@ -0,0 +1,6 @@
+console.log(Number('1' +'2'+'3')-Number(true))
+
+console.log(Boolean("false"))
+console.log(Boolean(6%2))
+console.log(Boolean(NaN)||Boolean('0'))
+console.log(Boolean(typeof false))
