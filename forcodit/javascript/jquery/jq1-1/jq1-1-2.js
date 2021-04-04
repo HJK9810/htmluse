c4c800f9fa8639b4f5c82b92796299dc9f929fe7
HJK9810 <ruby45342@gamail.com>
2021-04-04 15:07:18 +0900

    0404 codeit study

---
+++ forcodit/javascript/jquery/jq1-1/jq1-1-2.js
@@ -0,0 +1,18 @@
+// 주어진 단어(word)에 특정 알파벳(ch)이 몇 번 들어가는지 세어주는 함수
+function countCharacter(word, ch) {
+  var count = 0;
+
+  // 코드를 작성해주세요.
+
+  return count;
+}
+
+// 단어 word에 알파벳 'A'가 몇 번 나오는지 세어주는 함수
+function countA(word) {
+  // 코드를 작성해주세요.
+}
+
+// 테스트 코드
+console.log(countCharacter('AbaCedEA', 'E'));
+console.log(countCharacter('AbaCedEA', 'X'));
+console.log(countA('AbaCedEA'));
