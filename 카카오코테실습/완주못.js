807753a6b2a72ba5def351d7b00430e97282170b
HJK9810 <ruby45342@gamail.com>
2021-05-14 15:58:43 +0900

    sort를 이용한 간략화

--- 카카오코테실습/완주못.js
+++ 카카오코테실습/완주못.js
@@ -2,13 +2,16 @@ function solution(participant, completion) {
   let answer = '';
   let N = participant.length
 
-  for(let i=0;i<N;i++) {
-    completion.includes(participant[i]) ? participant[i] = '' : answer = participant[i];
-    console.log(`answer: ${answer}, el: ${participant[i]}`);
-  }
+  // 양쪽모두 내림차순 정렬
+  participant.sort();
+  completion.sort();
 
-  console.log(participant)
-  return answer;
+  // 정렬한것을 비교하며 불일치 찾기
+  for (let i = 0; i < N; i++) {
+    if (participant[i] != completion[i]) {
+      return participant[i]
+    }
+  }
 }
 
 console.log(solution(["leo", "kiki", "eden"], ["eden", "kiki"]))
