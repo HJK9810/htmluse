99c86215b69bbe8b45b460d641296a97840ba48f
HJK9810 <ruby45342@gamail.com>
2021-05-16 16:33:36 +0900

    leve1 두개 더하기 & 문자열 마음대로 정렬

---
+++ 카카오코테실습/두개뽑아덧셈.js
@@ -0,0 +1,15 @@
+function solution(numbers) {
+  let answer = [];
+
+  for (let i = 0; i < numbers.length; i++) {
+    for (let j = i + 1; j < numbers.length; j++) {
+      answer.push(numbers[i] + numbers[j]);
+    }
+  }
+  let ans = new Set(answer);
+
+  return [...ans].sort((a, b) => a - b);
+}
+
+console.log(solution([2, 1, 3, 4, 1]))
+console.log(solution([5, 0, 2, 7]))
