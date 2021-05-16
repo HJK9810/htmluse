fc83a13aa1f3d3072902781708bad83501978604
HJK9810 <ruby45342@gamail.com>
2021-05-16 17:00:53 +0900

    tc에서 전부 틀려 길게 늘여씀

--- 카카오코테실습/string정렬.js
+++ 카카오코테실습/string정렬.js
@@ -1,6 +1,34 @@
 function solution(strings, n) {
-  return strings.sort((a, b) => a.localeCompare(b.charAt(n), ));
+  let answer = [];
+  let str = [];
+
+  let nstrng = strings.map(el => el.charAt(n));
+  nstrng.sort();
+
+  for (let i = 0; i < strings.length; i++) {
+    str[i] = strings.filter(el => nstrng[i] === el.charAt(n))
+  }
+
+  let j = 0;
+  while (j < strings.length) {
+    let len = str[j].length;
+
+    if (len >= 2) {
+      answer.push(str[j].sort());
+      j += len;
+    } else {
+      answer.push(str[j]);
+      j++;
+    }
+  }
+
+  return answer.join().split(',');
 }
 
-console.log(solution(["sun", "bed", "car"],	1))
-console.log(solution(["abce", "abcd", "cdx"],	2))
+// 한줄로 간략화
+// function solution(strings, n) {
+//     return strings.sort((a, b) => a.charAt(n) === b.charAt(n) ? a.localeCompare(b) : a[n].localeCompare(b[n]));
+// }
+
+console.log(solution(["sun", "bed", "car"], 1))
+console.log(solution(["abce", "abcd", "cdx"], 2))
