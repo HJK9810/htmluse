a458d67e35010e94842702d0fb3ba68488506443
HJK9810 <ruby45342@gamail.com>
2021-05-20 17:16:16 +0900

    tc패스 수정

--- programmers실습/연습문제/level1/체육복.js
+++ programmers실습/연습문제/level1/체육복.js
@@ -1,8 +1,20 @@
 function solution(n, lost, reserve) {
   let ary = new Array(n + 1).fill(1);
   ary[0] = 0;
-  lost.forEach(el => ary[el] = 0);
-  reserve.forEach(el => ary[el] = 2);
+  for (let i = 1; i <= n; i++) {
+    let r = 0, l = 3;
+    if (lost.includes(i)) {
+      ary[i] = 0;
+      l = 1;
+    }
+    if (reserve.includes(i)) {
+      ary[i] = 2;
+      r = 1;
+    }
+    if (r === l) {
+      ary[i] = 1;
+    }
+  }
 
   for (let i = 1; i <= n; i++) {
     if (ary[i] === 0 && ary[i - 1] === 2) {
@@ -13,6 +25,7 @@ function solution(n, lost, reserve) {
       ary[i] = 1;
     }
   }
+  console.log(ary)
 
   return ary.filter(el => el > 0).length;
 }
