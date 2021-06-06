a47e605e0c2ae04174126b8a109dce794adf47e3
HJK9810 <ruby45342@gamail.com>
2021-06-06 16:35:23 +0900

    stack을 이용해 효용성 패스

--- programmers실습/기타/짝지어제거.js
+++ programmers실습/기타/짝지어제거.js
@@ -1,3 +1,20 @@
+// stack을 이용한 활용 => 효율성 up
+const solution = s => {
+  let stack = [];
+  if (s.length % 2 === 1) {
+    return 0;
+  } else {
+    for (let i = 0; i < s.length; i++) {
+      if (stack.length === 0 || stack[stack.length - 1] !== s.charAt(i)) {
+        stack.push(s.charAt(i))
+      } else {
+        stack.pop()
+      }
+    }
+    return stack.length === 0 ? 1 : 0;
+  }
+};
+
 function solution(s) {
   let string = s.split('');
   if (string.length % 2 === 1) {
