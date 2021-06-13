d8d26172df302f5651bfa8138ff621f1b7287963
HJK9810 <ruby45342@gamail.com>
2021-06-13 10:02:12 +0900

    level2 - print

---
+++ programmers실습/연습문제/level2/print.js
@@ -0,0 +1,20 @@
+function solution(priorities, location) {
+  let answer = 0;
+  const stack = priorities.map((el, i) => ({
+    loca: i === location,
+    val: el
+  }));
+
+  while (true) {
+    const standard = stack.splice(0, 1)[0];
+    if (stack.some(el => el.val > standard.val)) {
+      stack.push(standard);
+    } else {
+      answer++;
+      if (standard.loca) return answer;
+    }
+  }
+}
+
+console.log(solution([2, 1, 3, 2], 2))
+console.log(solution([1, 1, 9, 1, 1, 1], 0))
