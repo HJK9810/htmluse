af8b3084cbf150e738a56fab7d0540743b84db7d
HJK9810 <ruby45342@gamail.com>
2021-07-02 14:19:01 +0900

    타 코드 참고

--- programmers실습/기타/끝말잇기.js
+++ programmers실습/기타/끝말잇기.js
@@ -9,19 +9,39 @@ function solution(n, words) {
   while (index != -1) {
     que.push(first);
     const last = first.charAt(first.length - 1);
-    index = words.findIndex(el => el.charAt(0) === last);
+    index = words.findIndex(el => last === el.charAt(0));
     first = words.splice(index, 1).join('');
 
-    if (que.find(el => el === first)) return [answer[0]+1, answer[1]];
+    if (que.find(el => el === first)) return [answer[0] + 1, answer[1]];
 
     if (answer[0] === n) {
       answer[0] = 1;
       answer[1] += 1;
     } else answer[0]++;
 
-    if (answer[0] * answer[1] === leng) return [0, 0];
+    if (leng === answer[0] * answer[1]) return [0, 0];
   }
   return answer;
+}
+
+// 타 코드 참고
+function solution(n, words) {
+  let stack = {};
+
+  for (let i = 0; i < words.length; i++) {
+    const word = words[i];
+
+    if (!stack[word]) {
+      if (i > 0 && words[i - 1].charAt(words[i - 1].length - 1) !== word.charAt(0)) {
+        return [(i % n) + 1, Math.ceil((i + 1) / n)];
+      } else {
+        stack[word] = word;
+      }
+    } else {
+      return [(i % n) + 1, Math.ceil((i + 1) / n)];
+    }
+  }
+  return [0, 0];
 }
 
 let word1 = ["tank", "kick", "know", "wheel", "land", "dream", "mother", "robot", "tank"]
