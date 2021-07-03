d8e1595ede5ce31e53ae39906b241abfc1450a52
HJK9810 <ruby45342@gamail.com>
2021-07-03 14:03:10 +0900

    tc실패 해결 - if 초반에 동일한 나라가 연달아 들어갈경우에서의 hit 오류 해결

--- programmers실습/카카오18/cache.js
+++ programmers실습/카카오18/cache.js
@@ -1,24 +1,20 @@
 function solution(cacheSize, cities) {
-  let answer = 5 * cacheSize;
-  let stack = cities.splice(0, cacheSize);
-
-  // for (let i = 0; i < cacheSize; i++) {
-  //   stack.push(cities.shift());
-  //   answer += 5;
-  // }
+  let answer = 0;
+  let stack = [];
 
   for (let i = 0; i < cities.length; i++) {
-    const idx = stack.findIndex(el => el === cities[i]);
+    const idx = stack.findIndex(el => el == cities[i].toLowerCase());
+
     if (idx !== -1) {
       stack.push(stack.splice(idx, 1));
       answer += 1;
     } else {
-      stack.shift();
-      stack.push(cities[i]);
+      stack.length === cacheSize ? stack.shift() : stack;
+      stack.push(cities[i].toLowerCase());
       answer += 5;
     }
   }
-  return answer;
+  return !cacheSize ? cities.length * 5 : answer;
 }
 
 let city1 = ["Jeju", "Pangyo", "Seoul", "NewYork", "LA", "Jeju", "Pangyo", "Seoul", "NewYork", "LA"]
