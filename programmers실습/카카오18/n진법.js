427cb199da5787f4067ebad18c7aa382928399c1
HJK9810 <ruby45342@gamail.com>
2021-07-03 14:34:47 +0900

    level2 - n진법

--- programmers실습/카카오18/n진법.js
+++ programmers실습/카카오18/n진법.js
@@ -1,24 +1,26 @@
 function solution(n, t, m, p) {
   let answer = '';
-  let stack = [];
-  let k = 1;
+  let stack = ['0', '0'];
+  let k = 0;
 
-  for (let i = 0; i < m * t; i++) {
+  for (let i = 1; i < m * t; i++) {
     i.toString(n).split('').forEach(el => stack.push(el));
-    if (stack[(k - 1) + p]) {
-      answer += stack[(k - 1) + p];
+    const num = m * k + p;
+
+    if (stack[num]) {
+      answer += stack[num];
       k++;
     }
-    if (answer.length === t) return answer;
+    if (answer.length === t) return answer.toUpperCase();
   }
 }
 
-console.log(solution(2, 4, 2, 1))
-console.log(solution(2, 4, 2, 1).length)
-console.log(solution(16, 16, 2, 1))
-console.log(solution(16, 16, 2, 1).length)
-console.log(solution(16, 16, 2, 2))
-console.log(solution(16, 16, 2, 2).length)
-// console.log(solution(2, 4, 2, 1) === "0111")
-// console.log(solution(16, 16, 2, 1) === "02468ACE11111111")
-// console.log(solution(16, 16, 2, 2) === "13579BDF01234567")
+// console.log(solution(2, 4, 2, 1))
+// console.log(solution(2, 4, 2, 1).length)
+// console.log(solution(16, 16, 2, 1))
+// console.log(solution(16, 16, 2, 1).length)
+// console.log(solution(16, 16, 2, 2))
+// console.log(solution(16, 16, 2, 2).length)
+console.log(solution(2, 4, 2, 1) === "0111")
+console.log(solution(16, 16, 2, 1) === "02468ACE11111111")
+console.log(solution(16, 16, 2, 2) === "13579BDF01234567")
