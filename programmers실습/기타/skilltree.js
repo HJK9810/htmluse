81077b3bd2bdaa19d39c4d6da549cc38421b4a0e
HJK9810 <ruby45342@gamail.com>
2021-07-17 09:22:40 +0900

    간결하게 전환, TC 통과

--- programmers실습/기타/skilltree.js
+++ programmers실습/기타/skilltree.js
@@ -1,24 +1,13 @@
 function solution(skill, skill_trees) {
   let answer = 0;
-  const st = skill.split('');
 
   for (let i = 0; i < skill_trees.length; i++) {
+    // 각 스킬들을 별개의 부분으로 분리
     const myS = skill_trees[i].split('');
-    let idx = -1;
-    let check = 0;
-
-    for (let j = 0; j < st.length; j++) {
-      const index = myS.indexOf(st[j]);
-
-      if (index > idx || idx === (st.length - 1)) {
-        idx = index;
-        check++;
-      } else {
-        check = 0;
-        break;
-      }
-    }
-    if (check) answer++;
+    // 스킬을 포함하는 모든 스킬들 추출
+    const str = myS.filter(el => skill.includes(el)).join('');
+    // 스킬을 추출된 길이만큼 분리, 비교. 같을경우 || 스킬트리에 해당하는 스킬 제로인 경우. 둘중 하나 참일경우 ++.
+    if (str === skill.substring(0, str.length) || !str.length) answer++;
   }
   return answer;
 }
