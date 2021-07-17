b6e737ec3aa8b9a31c81bdcc44ca73ccc12995f2
HJK9810 <ruby45342@gamail.com>
2021-07-17 15:40:32 +0900

    level2 - jum&warp

--- programmers실습/기타/level2/jump.js
+++ programmers실습/기타/level2/jump.js
@@ -1,13 +1,19 @@
-function solution(n){
-    var ans = 0;
-
-    return ans;
+function solution(n) {
+  let ans = 0;
+  while (n > 0) {
+    if (n % 2 === 0) n /= 2;
+    else {
+      n -= 1;
+      ans++;
+    }
+  }
+  return ans;
 }
 
 console.log(solution(5))
 console.log(solution(6))
 console.log(solution(5000))
 
-// console.log(solution(5)===2)
-// console.log(solution(6)===2)
-// console.log(solution(5000)===5)
+console.log(solution(5)===2)
+console.log(solution(6)===2)
+console.log(solution(5000)===5)
