62f5b2b71b4c5cef3798e6566067547dd096ece5
HJK9810 <ruby45342@gamail.com>
2021-07-17 14:13:09 +0900

    기타폴더 레밸별 정리

---
+++ programmers실습/기타/level2/jump.js
@@ -0,0 +1,13 @@
+function solution(n){
+    var ans = 0;
+
+    return ans;
+}
+
+console.log(solution(5))
+console.log(solution(6))
+console.log(solution(5000))
+
+// console.log(solution(5)===2)
+// console.log(solution(6)===2)
+// console.log(solution(5000)===5)
