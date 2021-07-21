59c8e3993d8132a74ec006ed9e0145322fd40089
HJK9810 <ruby45342@gamail.com>
2021-07-21 09:49:50 +0900

    주석첨부

--- programmers실습/카카오18/방금그곡.js
+++ programmers실습/카카오18/방금그곡.js
@@ -2,25 +2,29 @@ function solution(m, musicinfos) {
   let answer = [];
   let timelen = [];
   musicinfos.forEach(inform => {
+    // [시작시간, 종료시간, 제목, 곡정보]
     const [start, fisnish, title, info] = inform.split(',');
     const [sh, sm] = start.split(':');
     const [fh, fm] = fisnish.split(':');
+    // 진행된 시간 계산
     const time = (fh - sh) * 60 + (fm - sm);
+    // 진행된 시간만큼 들려진 곡 정보
     const check = info.repeat(time).slice(0, time);
     const idx = check.indexOf(m)
 
-    if (idx !== -1) {
-      if (check[idx + m.length] !== '#') {
+    if (idx !== -1) { // 곡 정보내에 들은 값이 존재한다
+      if (check[idx + m.length] !== '#') { // #이 있을경우 해당 일치값이 아니다.
         answer.push(title);
         timelen.push(time);
       }
     }
   });
-
+  // 길이가 1 === 일치하는것이 없을경우, 유일값 출력
   if(answer.length===1) return answer[0];
-  else {
+  else { // 일치값이 여러개일경우
     const ary = new Set(timelen);
     const max = Math.max(...timelen);
+    // 시간이 동일? 먼저값. 시간 다름? 가장 긴것(max값)
     return ary === 1? answer[0] : answer[timelen.indexOf(max)];
   }
 }
