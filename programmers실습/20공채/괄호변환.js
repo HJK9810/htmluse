e5ede20b8608201e6264597203ce69ed936dbf55
HJK9810 <ruby45342@gamail.com>
2021-07-28 14:36:50 +0900

    재귀를 이용한 체크, 확인

--- programmers실습/20공채/괄호변환.js
+++ programmers실습/20공채/괄호변환.js
@@ -1,38 +1,39 @@
 function solution(p) {
+  if (!p.length) return '';
+
   let answer = '';
-  let stak = [];
-  let putin = 0;
-  let popout = 0;
+  let left = 0;
+  let right = 0;
+  let check = false;
 
   for (let i = 0; i < p.length; i++) {
-    const a = p.charAt(i);
+    if (p.charAt(i) === '(') left++;
+    if (p.charAt(i) === ')') right++;
 
-    if (a === '(') {
-      if (putin === popout) {
-        stak.push(a);
-        answer += a;
-        putin++;
+    if (right > left) check = true; // 올바르지 않은 괄호
+
+    if (right === left) { // U와 V 쪼개기
+      if (check) {
+        answer = answer + '(' + solution(p.slice(i + 1)) + ')';
+        for (let j = 1; j < i; j++) {
+          if (p.charAt(j) === ')') answer += '(';
+          if (p.charAt(j) === '(') answer += ')';
+        }
+        return answer;
       } else {
-        stak.pop();
-        answer += ')';
-        popout++;
-      }
-    } else {
-      if (stak.length === 0) {
-        stak.push('(');
-        answer += '(';
-        putin++;
-      } else {
-        stak.pop();
-        answer += ')';
-        popout++;
+        answer = answer + p.slice(0, i + 1) + solution(p.slice(i + 1));
+        return answer;
       }
     }
   }
-
-  return answer;
 }
 
 console.log(solution("(()())()"))
 console.log(solution(")("))
 console.log(solution("()))((()"))
+console.log(solution(""))
+
+// console.log(solution("(()())()")==='(()())()')
+// console.log(solution(")(")==='()')
+// console.log(solution("()))((()")==='()(())()')
+// console.log(solution("")==='')
