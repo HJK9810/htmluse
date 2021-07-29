e65232caf98b981f375b8cfae6980dc7799bb5ab
HJK9810 <ruby45342@gamail.com>
2021-07-29 11:39:03 +0900

    반복이 가능하도록 배열분리

--- programmers실습/20인턴/수식최대화.js
+++ programmers실습/20인턴/수식최대화.js
@@ -1,55 +1,49 @@
-const ranks = [
-  ['*', '+', '-'],
-  ['*', '-', '+'],
-  ['+', '*', '-'],
-  ['+', '-', '*'],
-  ['-', '*', '+'],
-  ['-', '+', '*']
-];
+const ranks = ['*+-', '*-+', '+*-', '+-*', '-*+', '-+*'];
 
 function solution(expression) {
   let answer = 0;
-  const numbers = expression.split(/[*+-]/g);
-  const oper = expression.match(/[*+-]/g);
+  const splitstring = expression.split(/(\D)/); // 숫자와 기호 분리
 
-  ranks.forEach(rank => {
-    let nums = [...numbers];
+  for (let index = 0; index < ranks.length; index++) {
+    const rank = ranks[index].split('');
+    let numbers = [...splitstring]; // 반복을 위해 배열 분리
+
     for (let i = 0; i < 3; i++) {
-      const checki = [];
-      oper.forEach((el, idx) => {if(el === rank[i]) return checki.push(idx);});
+      // 전체 길이의 절반을 나눴을때, 소숫점 아래를 버리면 기호들의 수와 동일하다.
+      const operleng = Math.floor(numbers.length / 2);
 
-      console.log(`checki : ${checki}`)
+      for (let j = 0; j < operleng; j++) {
+        const idx = numbers.indexOf(rank[i]);
+        let sum = 0;
 
-      for (let j = 0; j < checki.length; j++) {
-        switch (rank[i]) {
-          case '*':
-            sum = Number(numbers[checki[j]]) * Number(numbers[checki[j] + 1]);
-            break;
-          case '+':
-            sum = Number(numbers[checki[j]]) + Number(numbers[checki[j] + 1]);
-            break;
-          case '-':
-            sum = Number(numbers[checki[j]]) - Number(numbers[checki[j] + 1]);
-            break;
-          default:
+        if (idx !== -1) {
+          switch (rank[i]) {
+            case '*':
+              sum = Number(numbers[idx - 1]) * Number(numbers[idx + 1]);
+              break;
+            case '+':
+              sum = Number(numbers[idx - 1]) + Number(numbers[idx + 1]);
+              break;
+            case '-':
+              sum = Number(numbers[idx - 1]) - Number(numbers[idx + 1]);
+              break;
+            default:
+          }
+
+          numbers.splice(idx - 1, 3, sum); // 계산된 값, 합계값과 교체
         }
-
-        nums.splice(checki[j], 2, sum, '');
-        console.log(`nums: ${nums}`);
       }
     }
 
-    if (answer < Math.abs(nums[0])) answer = Math.abs(nums[0]);
-    console.log(answer)
-  });
-
+    if (answer < Math.abs(numbers[0])) answer = Math.abs(numbers[0]);
+  }
   return answer;
 }
 
 console.log(solution("100-200*300-500+20"))
-// console.log(solution("50*6-3*2"))
-// console.log(solution("200-300-500-600*40+500+500"))
+console.log(solution("50*6-3*2"))
+console.log(solution("200-300-500-600*40+500+500"))
 
-// console.log(solution("100-200*300-500+20")===60420)
-// console.log(solution("50*6-3*2")===300)
-// console.log(solution("200-300-500-600*40+500+500")===1248000)
+console.log(solution("100-200*300-500+20") === 60420)
+console.log(solution("50*6-3*2") === 300)
+console.log(solution("200-300-500-600*40+500+500") === 1248000)
