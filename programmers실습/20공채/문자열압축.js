69cfcbb1588c861a558f2b72351c7415c3cd32b4
HJK9810 <ruby45342@gamail.com>
2021-07-30 14:29:08 +0900

    level2-문자열 압축

--- programmers실습/20공채/문자열압축.js
+++ programmers실습/20공채/문자열압축.js
@@ -4,29 +4,40 @@ function solution(s) {
 
   for (let i = 1; i <= len; i++) {
     let string = '';
-    for (let j = 0; j < s.length; j++) {
-      let count = 0;
-      let stand = s.substring(j, j + i);
-      let check;
+    let j = 0;
+    while (j < s.length) {
+      let count = 1;
+      let maxium = j + i;
+      let stand = s.substring(j, maxium);
+      let check = s.substring(maxium, maxium + i);
 
-      if (count) string += (count + stand);
-      else string += stand;
+      while (stand === check) {
+        maxium += i;
+        check = s.substring(maxium, maxium + i);
+        count++;
+      }
+
+      if (count > 1) {
+        string += (count + stand);
+        j += count * i;
+      } else {
+        string += stand;
+        j += i;
+      }
     }
-    console.log(`string: ${string}`)
     answer.push(string.length);
   }
-  console.log(answer)
   return Math.min(...answer);
 }
 
-console.log(solution("aabbaccc"))
+// console.log(solution("aabbaccc"))
 // console.log(solution("ababcdcdababcdcd"))
 // console.log(solution("abcabcdede"))
 // console.log(solution("abcabcabcabcdededededede"))
 // console.log(solution("xababcdcdababcdcd"))
 
-// console.log(solution("aabbaccc")===7)
-// console.log(solution("ababcdcdababcdcd")===9)
-// console.log(solution("abcabcdede")===8)
-// console.log(solution("abcabcabcabcdededededede")===14)
-// console.log(solution("xababcdcdababcdcd")===17)
+console.log(solution("aabbaccc") === 7)
+console.log(solution("ababcdcdababcdcd") === 9)
+console.log(solution("abcabcdede") === 8)
+console.log(solution("abcabcabcabcdededededede") === 14)
+console.log(solution("xababcdcdababcdcd") === 17)
