8fb6a5d5eea23a75f39b6c41d6f469255003e223
HJK9810 <ruby45342@gamail.com>
2021-08-02 09:34:00 +0900

    min값이 col값과 동일해 for문 튕겨지는 경우

--- programmers실습/카카오19/후보키.js
+++ programmers실습/카카오19/후보키.js
@@ -1,21 +1,20 @@
 function solution(relation) {
   let answer = 0;
   let len = relation.length;
-  let nMax = relation[0].length;
+  let col = relation[0].length;
 
-  for (let n = 0; n < nMax; n++) {
+  for (let n = 0; n < col; n++) {
     let check = new Set();
     for (let i = 0; i < len; i++) {
       check.add(relation[i][n]);
     }
 
     if (check.size !== len) {
-      for (let count = 2; n + count < nMax; count++) {
+      for (let count = 2; n + count <= col; count++) {
         check.clear();
         for (let i = 0; i < len; i++) {
           check.add(relation[i].splice(n, count).join(''));
         }
-
         if (check.size === len) {
           answer++;
           break;
@@ -27,8 +26,8 @@ function solution(relation) {
 }
 
 let rel1 = [["100", "ryan", "music", "2"], ["200", "apeach", "math", "2"], ["300", "tube", "computer", "3"], ["400", "con", "computer", "4"], ["500", "muzi", "music", "3"], ["600", "apeach", "music", "2"]]
-let rel2 = [["a", "aa"], ["aa", "a"], ["a,a"]]
-console.log(solution(rel1))
+let rel2 = [["a", "aa"], ["aa", "a"], ["a", "a"]]
+// console.log(solution(rel1))
 console.log(solution(rel2))
 
 // console.log(solution(rel1) === 2)
