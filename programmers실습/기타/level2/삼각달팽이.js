1dd792c2864869c067f1c97b8ceb23b88eb0c708
HJK9810 <ruby45342@gamail.com>
2021-08-05 09:23:47 +0900

    level2-삼각달팽이

---
+++ programmers실습/기타/level2/삼각달팽이.js
@@ -0,0 +1,32 @@
+function solution(n) {
+  let answer = [];
+  for (let i = 0; i < n; i++) {
+    answer.push(Array.from({ length: i + 1 }, v => 0));
+  }
+  let i = 1;
+  answer[0][0] = 1;
+  const DOWN = (x, y) => {
+    const next = answer[x][y + 1];
+    if (next === 0) next = i + 1;
+    else if (answer[x + 1][y] !== 0) return answer;
+    else RIGHT(x, y);
+  }
+  const RIGHT = (x, y) => {
+    const next = answer[x + 1][y];
+    if (next === 0) next = i + 1;
+    else if (answer[x - 1][y + 1] !== 0) return answer;
+    else UP(x, y);
+  }
+  const UP = (x, y) => {
+    const next = answer[x][y + 1];
+    if (next === 0) next = i + 1;
+    else if (answer[x + 1][y] !== 0) return answer;
+    else DOWN(x, y);
+  }
+
+  DOWN(0, 0);
+}
+
+console.log(solution(4))
+console.log(solution(5))
+console.log(solution(6))
