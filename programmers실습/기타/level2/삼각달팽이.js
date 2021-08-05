3a04dcf0f46196159c7ed356c7bc98650e3d4b92
HJK9810 <ruby45342@gamail.com>
2021-08-05 14:13:18 +0900

    개별함수 위치상 오류를 내부의 인스턴트 함수를 사용하여 해결

--- programmers실습/기타/level2/삼각달팽이.js
+++ programmers실습/기타/level2/삼각달팽이.js
@@ -7,22 +7,38 @@ function solution(n) {
   answer[0][0] = 1;
   const DOWN = (x, y) => {
     const next = answer[x][y + 1];
-    if (next === 0) next = i + 1;
+    if (next === 0) { next = i + 1; i++; }
     else if (answer[x + 1][y] !== 0) return answer;
-    else RIGHT(x, y);
+    else (a = x, b = y) => {
+      const next = answer[a + 1][b];
+      if (next === 0) { next = i + 1; i++; }
+      else if (answer[a - 1][b + 1] !== 0) return answer;
+      else (x = a, y = b) => {
+        const next = answer[x][y + 1];
+        if (next === 0) { next = i + 1; i++; }
+        else if (answer[x + 1][y] !== 0) return answer;
+        else DOWN(x, y);
+      }
+    }
   }
-  const RIGHT = (x, y) => {
-    const next = answer[x + 1][y];
-    if (next === 0) next = i + 1;
-    else if (answer[x - 1][y + 1] !== 0) return answer;
-    else UP(x, y);
-  }
-  const UP = (x, y) => {
-    const next = answer[x][y + 1];
-    if (next === 0) next = i + 1;
-    else if (answer[x + 1][y] !== 0) return answer;
-    else DOWN(x, y);
-  }
+  // const DOWN = (x, y) => {
+  //   const next = answer[x][y + 1];
+  //   if (next === 0) {next = i + 1; i++;}
+  //   else if (answer[x + 1][y] !== 0) return answer;
+  //   else RIGHT(x, y);
+  // }
+  // const RIGHT = (x, y) => {
+  //   const next = answer[x + 1][y];
+  //   if (next === 0) {next = i + 1; i++;}
+  //   else if (answer[x - 1][y + 1] !== 0) return answer;
+  //   else UP(x, y);
+  // }
+  // const UP = (x, y) => {
+  //   const next = answer[x][y + 1];
+  //   if (next === 0) {next = i + 1; i++;}
+  //   else if (answer[x + 1][y] !== 0) return answer;
+  //   else DOWN(x, y);
+  // }
 
   DOWN(0, 0);
 }
