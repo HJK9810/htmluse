b2e289f1c8f44a0f1c7f2917223ccd3e0b0bef01
HJK9810 <ruby45342@gamail.com>
2021-08-06 09:24:25 +0900

    함수가 아닌 for문을 사용해 해결.

--- programmers실습/기타/level2/삼각달팽이.js
+++ programmers실습/기타/level2/삼각달팽이.js
@@ -1,46 +1,21 @@
 function solution(n) {
+  // let answer = Array.from({length:n}, (v, i)=>Array.from({ length: i + 1 }, el => 0));
   let answer = [];
   for (let i = 0; i < n; i++) {
     answer.push(Array.from({ length: i + 1 }, v => 0));
   }
-  let i = 1;
-  answer[0][0] = 1;
-  const DOWN = (x, y) => {
-    const next = answer[x][y + 1];
-    if (next === 0) { next = i + 1; i++; }
-    else if (answer[x + 1][y] !== 0) return answer;
-    else (a = x, b = y) => {
-      const next = answer[a + 1][b];
-      if (next === 0) { next = i + 1; i++; }
-      else if (answer[a - 1][b + 1] !== 0) return answer;
-      else (x = a, y = b) => {
-        const next = answer[x][y + 1];
-        if (next === 0) { next = i + 1; i++; }
-        else if (answer[x + 1][y] !== 0) return answer;
-        else DOWN(x, y);
-      }
-    }
-  }
-  // const DOWN = (x, y) => {
-  //   const next = answer[x][y + 1];
-  //   if (next === 0) {next = i + 1; i++;}
-  //   else if (answer[x + 1][y] !== 0) return answer;
-  //   else RIGHT(x, y);
-  // }
-  // const RIGHT = (x, y) => {
-  //   const next = answer[x + 1][y];
-  //   if (next === 0) {next = i + 1; i++;}
-  //   else if (answer[x - 1][y + 1] !== 0) return answer;
-  //   else UP(x, y);
-  // }
-  // const UP = (x, y) => {
-  //   const next = answer[x][y + 1];
-  //   if (next === 0) {next = i + 1; i++;}
-  //   else if (answer[x + 1][y] !== 0) return answer;
-  //   else DOWN(x, y);
-  // }
+  let num = 0;
+  let x = -1;
+  let y = 0;
 
-  DOWN(0, 0);
+  while (n > 0) {
+    for (let i = 0; i < n; i++) answer[++x][y] = ++num;
+    for (let i = 0; i < n - 1; i++) answer[x][++y] = ++num;
+    for (let i = 0; i < n - 2; i++) answer[--x][--y] = ++num;
+    n -= 3;
+  }
+
+  return answer.flat();
 }
 
 console.log(solution(4))
