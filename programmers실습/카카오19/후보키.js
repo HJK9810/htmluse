cdb08b37f4202072b1deb65aaf261dad15ad8229
HJK9810 <ruby45342@gamail.com>
2021-08-16 09:09:48 +0900

    dfs와 fillter를 사용

--- programmers실습/카카오19/후보키.js
+++ programmers실습/카카오19/후보키.js
@@ -1,33 +1,65 @@
 function solution(relation) {
-  let answer = 0;
-  let len = relation.length;
   let col = relation[0].length;
+  let keyList = [];
+  let combList = [];
 
-  for (let n = 0; n < col; n++) {
-    let check = new Set();
-    for (let i = 0; i < len; i++) {
-      check.add(relation[i][n]);
+  // 조합배열용
+  const comb = (arr, idx, r, target) => {
+    if (r === 0) combList.push(Object.assign([], arr));
+    else if (target === col) return;
+    else {
+      arr[idx] = target;
+      comb(arr, idx + 1, r - 1, target + 1);
+      comb(arr, idx, r, target + 1);
     }
-
-    if (check.size !== len) {
-      for (let count = 2; n + count <= col; count++) {
-        check.clear();
-        for (let i = 0; i < len; i++) {
-          check.add(relation[i].splice(n, count).join(''));
-        }
-        if (check.size === len) {
-          answer++;
-          break;
-        }
-      }
-    } else answer++;
   }
-  return answer;
+
+  for (let i = 1; i <= col; i++) {
+    comb([], 0, i, 0);
+  }
+
+  for (let i = 0; i < combList.length; i++) {
+    addKey(keyList, combList[i], relation);
+  }
+  return keyList.length;
+}
+
+// 후보키 체크
+const addKey = (keyList, key, relate) => {
+  // 최소성 체크
+  let check = true;
+  for (let i = 0; i < keyList.length; i++) {
+    let now = keyList[i];
+    for (let j = 0; j < key.length; j++) {
+      now = now.filter(el => el !== key[j]);
+    }
+    check = now.length !== 0 ? true : false;
+  }
+
+  if (keyList.length !== 0 && !check) return;
+
+  let ary = [];
+  let unique = true;
+  // 유일성 체크
+  for (let i = 0; i < relate.length; i++) {
+    let findel = ary.find(el => {
+      let flag = true;
+      for (let j = 0; j < key.length; j++) {
+        if (el[key[j]] !== relate[j][key[j]]) flag = false;
+      }
+      return flag;
+    });
+
+    if (findel !== undefined) unique = false;
+    else ary.push(relate[i]);
+  }
+
+  if (unique) keyList.push(key);
 }
 
 let rel1 = [["100", "ryan", "music", "2"], ["200", "apeach", "math", "2"], ["300", "tube", "computer", "3"], ["400", "con", "computer", "4"], ["500", "muzi", "music", "3"], ["600", "apeach", "music", "2"]]
 let rel2 = [["a", "aa"], ["aa", "a"], ["a", "a"]]
-// console.log(solution(rel1))
+console.log(solution(rel1))
 console.log(solution(rel2))
 
 // console.log(solution(rel1) === 2)
