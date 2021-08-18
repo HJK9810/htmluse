ae373ebc3d05569c9facae79da66dfd8368cfe7c
HJK9810 <ruby45342@gamail.com>
2021-08-18 10:42:39 +0900

    비트마스크를 이용 >> 참고한 사이트 기술

--- programmers실습/카카오19/후보키.js
+++ programmers실습/카카오19/후보키.js
@@ -1,62 +1,37 @@
+const getSet = [];
+const combSet = new Set();
+
 function solution(relation) {
-  let col = relation[0].length;
-  let keyList = [];
-  let combList = [];
-
-  // 조합배열용
-  const comb = (arr, idx, r, target) => {
-    if (r === 0) combList.push(Object.assign([], arr));
-    else if (target === col) return;
-    else {
-      arr[idx] = target;
-      comb(arr, idx + 1, r - 1, target + 1);
-      comb(arr, idx, r, target + 1);
-    }
+  const col = relation[0].length;
+  for (let i = 1; i < 1 << col; i++) {
+    if (!checkCan(i, relation)) continue;
+    if (checkSet(i)) continue;
+    getSet.push(i);
   }
-
-  for (let i = 1; i <= col; i++) {
-    comb([], 0, i, 0);
-  }
-
-  for (let i = 0; i < combList.length; i++) {
-    addKey(keyList, combList[i], relation);
-  }
-  console.log(keyList)
-  return keyList.length;
+  return getSet.length;
 }
 
-// 후보키 체크
-const addKey = (keyList, key, relate) => {
-  // 최소성 체크
-  let check = true;
-  for (let i = 0; i < keyList.length; i++) {
-    let now = keyList[i];
-    for (let j = 0; j < key.length; j++) {
-      now = now.filter(el => el !== key[j]);
+const checkCan = (key, relation) => {
+  const checkSet = new Set();
+  const col = relation[0].length;
+  for (let i = 0; i < relation.length; i++) {
+    let str = '';
+    for (let j = 0; j < col; j++) {
+      if (key & 1 << j) str += relation[i][j];
     }
-    if(now.length === 0) check = false;
+    if (checkSet.has(str)) return false;
+    checkSet.add(str);
   }
-
-  if (keyList.length !== 0 && !check) return;
-
-  let ary = [];
-  let unique = true;
-  // 유일성 체크
-  for (let i = 0; i < relate.length; i++) {
-    let findel = ary.find(el => {
-      let flag = true;
-      for (let j = 0; j < key.length; j++) {
-        if (el[key[j]] !== relate[j][key[j]]) flag = false;
-      }
-      return flag;
-    });
-
-    if (findel !== undefined) unique = false;
-    else ary.push(relate[i]);
+  return true;
+};
+const checkSet = key => {
+  for (let candid of getSet) {
+    if ((key & candid) === candid) return true;
   }
+  return false;
+};
 
-  if (unique) keyList.push(key);
-}
+// 참고 : https://hongnewhonglog.tistory.com/43
 
 let rel1 = [["100", "ryan", "music", "2"], ["200", "apeach", "math", "2"], ["300", "tube", "computer", "3"], ["400", "con", "computer", "4"], ["500", "muzi", "music", "3"], ["600", "apeach", "music", "2"]]
 let rel2 = [["a", "aa"], ["aa", "a"], ["a", "a"]]
