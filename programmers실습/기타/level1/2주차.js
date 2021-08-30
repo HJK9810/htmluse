33f6ad387ff7fe471d69447494072a2ac5e6196e
HJK9810 <ruby45342@gamail.com>
2021-08-30 10:16:31 +0900

    예제추가 & 열 추출 for문으로 교체

--- programmers실습/기타/level1/2주차.js
+++ programmers실습/기타/level1/2주차.js
@@ -8,10 +8,18 @@ const grade = (num) => {
 
 function solution(scores) {
   let answer = '';
-  const Score = scores.reduce((result, row) => row.map((e, i) => [...(result[i] || []), e]), []);
-  for (let i = 0; i < Score.length; i++) {
-    let ary = Score[i];
-    if (Math.max(...ary) === ary[i] || Math.min(...ary) === ary[i]) ary.splice(i, 1);
+  for (let i = 0; i < scores.length; i++) {
+    let ary = [];
+    for (let j = 0; j < scores.length; j++) {
+      ary.push(scores[j][i]);
+    }
+    console.log(ary)
+
+    const max = Math.max(...ary);
+    const min = Math.min(...ary);
+    if (max === ary[i] && (ary.indexOf(max, i + 1) === -1 || ary.indexOf(max) <= i) || min === ary[i] && (ary.indexOf(min, i + 1) === -1 || ary.indexOf(min) <= i)) ary.splice(i, 1);
+
+    console.log(ary)
     let aver = ary.reduce((r, e) => r + e, 0) / ary.length;
     answer += grade(aver);
   }
@@ -21,7 +29,14 @@ function solution(scores) {
 let ary1 = [[100, 90, 98, 88, 65], [50, 45, 99, 85, 77], [47, 88, 95, 80, 67], [61, 57, 100, 80, 65], [24, 90, 94, 75, 65]];
 let ary2 = [[50, 90], [50, 87]];
 let ary3 = [[70, 49, 90], [68, 50, 38], [73, 31, 100]];
+let ary4 = [[75, 50, 100], [75, 100, 20], [100, 100, 20]];
 
-console.log(solution(ary1))
-console.log(solution(ary2))
-console.log(solution(ary3))
+// console.log(solution(ary1))
+// console.log(solution(ary2))
+// console.log(solution(ary3))
+console.log(solution(ary4)) 
+
+// console.log(solution(ary1)=="FBABD")
+// console.log(solution(ary2)=="DA")
+// console.log(solution(ary3)=="CFD")
+console.log(solution(ary4)=="BBF") 
