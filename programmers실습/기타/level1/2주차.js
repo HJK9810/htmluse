43e734944fec6513282f92d100b93e74189b9329
HJK9810 <ruby45342@gamail.com>
2021-09-03 09:15:39 +0900

    동일인덱스 앞 뒤로 동일 최소 or 최대값 존재시 false

--- programmers실습/기타/level1/2주차.js
+++ programmers실습/기타/level1/2주차.js
@@ -17,7 +17,7 @@ function solution(scores) {
 
     const max = Math.max(...ary);
     const min = Math.min(...ary);
-    if (max === ary[i] && (ary.indexOf(max, i + 1) === -1 || ary.indexOf(max) <= i) || min === ary[i] && (ary.indexOf(min, i + 1) === -1 || ary.indexOf(min) <= i)) ary.splice(i, 1);
+    if (max === ary[i] && (ary.indexOf(max, i + 1) === -1 || ary.slice(0, i - 1).indexOf(max) === -1) || min === ary[i] && (ary.indexOf(min, i + 1) === -1 || ary.slice(0, i - 1).indexOf(min) === -1)) ary.splice(i, 1);
 
     console.log(ary)
     let aver = ary.reduce((r, e) => r + e, 0) / ary.length;
@@ -34,9 +34,9 @@ let ary4 = [[75, 50, 100], [75, 100, 20], [100, 100, 20]];
 // console.log(solution(ary1))
 // console.log(solution(ary2))
 // console.log(solution(ary3))
-console.log(solution(ary4)) 
+console.log(solution(ary4))
 
 // console.log(solution(ary1)=="FBABD")
 // console.log(solution(ary2)=="DA")
 // console.log(solution(ary3)=="CFD")
-console.log(solution(ary4)=="BBF") 
+// console.log(solution(ary4)=="BBF") 
