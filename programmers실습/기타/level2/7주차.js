5ae7a1c80669f5dda1da127417c7b3e3f857b69e
HJK9810 <ruby45342@gamail.com>
2021-09-13 15:19:20 +0900

    무한루프 제거

--- programmers실습/기타/level2/7주차.js
+++ programmers실습/기타/level2/7주차.js
@@ -1,8 +1,8 @@
 function solution(enter, leave) {
-  let answer = [];
+  let answer = Array.from({ length: enter.length }, () => 0);
   let stack = [];
 
-  while (leave.length > 0) {
+  for (let i = 0; i < enter.length + leave.length; i++) {
     if (enter.length > 0) {
       const sted = enter.shift();
       stack.push(sted);
@@ -11,7 +11,7 @@ function solution(enter, leave) {
     if (stack.indexOf(leave[0]) !== -1) {
       const lsted = leave.shift();
       const idx = stack.indexOf(lsted);
-      stack = stack.splice(idx, 1);
+      stack.splice(idx, 1);
       answer[lsted - 1] = stack.length;
     }
   }
@@ -20,9 +20,9 @@ function solution(enter, leave) {
 }
 
 console.log(solution([1, 3, 2], [1, 2, 3]))
-console.log(solution([1, 4, 2, 3], [2, 1, 3, 4]))
-console.log(solution([3, 2, 1], [2, 1, 3]))
-console.log(solution([3, 2, 1], [1, 3, 2]))
-console.log(solution([1, 4, 2, 3], [2, 1, 4, 3]))
+// console.log(solution([1, 4, 2, 3], [2, 1, 3, 4]))
+// console.log(solution([3, 2, 1], [2, 1, 3]))
+// console.log(solution([3, 2, 1], [1, 3, 2]))
+// console.log(solution([1, 4, 2, 3], [2, 1, 4, 3]))
 
 	// [0,1,1]	[2,2,1,3] 	[1,1,2]	[2,2,2]	[2,2,0,2]
