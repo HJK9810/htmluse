5a1a42bc11c28b0b7ea107aa4704576f063a3053
HJK9810 <ruby45342@gamail.com>
2021-09-13 14:33:43 +0900

    블로그 참고를 통해 해결

--- programmers실습/21공채/메뉴리뉴얼.js
+++ programmers실습/21공채/메뉴리뉴얼.js
@@ -1,48 +1,47 @@
 function solution(orders, course) {
-  let answer = [];
-  let menu = [];
+  let candid = {}; // 함수 실행을 위해 변수 생성
 
-  // menu 분리
-  for (let j = 0; j < orders.length; j++) {
-    orders[j] = orders[j].split('').sort().join('');
-    let order = orders[j];
-    for (let i = 0; i < order.length; i++) {
-      if (menu.indexOf(order.charAt(i)) === -1) menu.push(order.charAt(i));
+  const answer = course.reduce((answer, num) => {
+    candid = {}; // 초기화
+
+    for (let i = 0; i < orders.length; i++) {
+      if (orders[i] < num) continue;
+
+      combinate(orders[i].split('').sort(), '', orders[i].length, num, 0);
+    }
+
+    let menu = [];
+    let mosted = 0;
+    for (let key in candid) {
+      let value = candid[key];
+      if (value > 1) {
+        if (value > mosted) {
+          mosted = value;
+          menu = [key];
+        } else if (value === mosted) menu.push(key);
+      }
+    }
+
+    mosted = 0;
+    return [...answer, ...menu];
+  }, []);
+
+  function combinate (arr, target, n, r, count) {
+    if (r === 0) {
+      let cnt = candid[target] || 0;
+      candid[target] = cnt + 1;
+    } else if (n === 0 || n < r) return;
+    else {
+      combinate(arr, target + arr[count], n - 1, r - 1, count + 1);
+      combinate(arr, target, n - 1, r, count + 1);
     }
   }
 
-  // 가능한 코스 매칭 멱집합 사용.
-  const subsets = (ary) => {
-    const res = [];
-
-    const dfs = (start, arr) => {
-      res.push(arr.join(''));
-
-      if (arr.length === ary) return;
-
-      for (let i = start; i < ary.length; i++) {
-        dfs(i + 1, [...arr, ary[i]]);
-      }
-    };
-    dfs(0, []);
-
-    return res;
-  };
-  let sets = subsets(menu);
-  let cors = [];
-  for (let i = 0; i < course.length; i++) {
-    cors = cors.concat(sets.filter(el => el.length === course[i]));
-  }
-
-  // 모든 코스들중 2명 이상 섭취한것 고르기
-  let coursecheck = {};
-  for (let i = 0; i < cors.length; i++) {
-    const filtlen = orders.filter(el => el.indexOf(cors[i]) !== -1).length;
-    if (filtlen >= 2) coursecheck[cors[i]] = filtlen;
-  }
-  return coursecheck;
+  return answer.sort();
 }
 
 console.log(solution(["ABCFG", "AC", "CDE", "ACDE", "BCFG", "ACDEH"], [2, 3, 4]))
 console.log(solution(["ABCDE", "AB", "CD", "ADE", "XYZ", "XYZ", "ACD"], [2, 3, 5]))
 console.log(solution(["XYZ", "XWY", "WXA"], [2, 3, 4]))
+
+// https://after-newmoon.tistory.com/98 참고
