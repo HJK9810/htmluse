aeb70a0f549a809660662bc24bc176cd6037ed40
HJK9810 <ruby45342@gamail.com>
2021-09-14 14:24:51 +0900

    인원 체크시 중복체크를 set으로 방지

--- programmers실습/기타/level2/7주차.js
+++ programmers실습/기타/level2/7주차.js
@@ -1,28 +1,43 @@
 function solution(enter, leave) {
-  let answer = Array.from({ length: enter.length }, () => 0);
+  let answer = [];
+  let anstack = Array.from({ length: enter.length }, () => new Set);
   let stack = [];
 
-  for (let i = 0; i < enter.length + leave.length; i++) {
+  for (let count = 0; count <= enter.length + leave.length; count++) {
     if (enter.length > 0) {
       const sted = enter.shift();
       stack.push(sted);
     }
 
-    if (stack.indexOf(leave[0]) !== -1) {
-      const lsted = leave.shift();
-      const idx = stack.indexOf(lsted);
-      stack.splice(idx, 1);
-      answer[lsted - 1] = stack.length;
+    if (stack.length === 1 && stack[0] === leave[0]) {
+      stack.pop();
+      leave.shift();
+    } else {
+      while (stack.indexOf(leave[0]) !== -1) {
+        for (let i = 0; i < stack.length; i++) {
+          for (let j = 0; j < stack.length; j++) {
+            anstack[stack[i] - 1].add(stack[j]);
+          }
+        }
+        const lsted = leave.shift();
+        const idx = stack.indexOf(lsted);
+        stack.splice(idx, 1);
+      }
     }
+  }
+
+  for (let i = 0; i < anstack.length; i++) {
+    anstack[i].size ? answer.push(anstack[i].size - 1) : answer.push(0);
   }
 
   return answer;
 }
 
 console.log(solution([1, 3, 2], [1, 2, 3]))
-// console.log(solution([1, 4, 2, 3], [2, 1, 3, 4]))
-// console.log(solution([3, 2, 1], [2, 1, 3]))
-// console.log(solution([3, 2, 1], [1, 3, 2]))
-// console.log(solution([1, 4, 2, 3], [2, 1, 4, 3]))
+console.log(solution([1, 4, 2, 3], [2, 1, 3, 4]))
+console.log(solution([3, 2, 1], [2, 1, 3]))
+console.log(solution([3, 2, 1], [1, 3, 2]))
+console.log(solution([1, 4, 2, 3], [2, 1, 4, 3]))
+console.log(solution([1, 2, 3], [3, 2, 1]))
 
-	// [0,1,1]	[2,2,1,3] 	[1,1,2]	[2,2,2]	[2,2,0,2]
+console.log(`[0,1,1]\n [2,2,1,3]\n [1,1,2]\n [2,2,2]\n	[2,2,0,2]\n [2, 2, 2]\n`)
