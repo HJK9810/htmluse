7c091999c58f78ac82860aa4010ff9e4b575666a
HJK9810 <ruby45342@gamail.com>
2021-10-16 09:38:37 +0900

    level2-n^2배열 자르기

---
+++ programmers실습/기타/level2/n^2배열자르기.js
@@ -0,0 +1,19 @@
+function solution(n, left, right) {
+  let array = Array.from({ length: n }, () => Array(n).fill(null));
+
+  for (let i = 0; i < n; i++) {
+    for (let j = 0; j <= i; j++) {
+      for (let k = 0; k <= i; k++) {
+        if (!array[j][k]) array[j][k] = i + 1;
+      }
+    }
+  }
+
+  let answer = array.flat()
+  return answer.slice(left, right + 1);
+}
+
+
+console.log(solution(3, 2, 5))
+console.log(solution(4, 7, 14))
+console.log(`[3,2,2,3]\n[4,3,3,3,4,4,4,4]`)
