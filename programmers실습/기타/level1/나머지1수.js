8ba7ac2d1d0754efda8f8d5e30d4f999651bfa52
HJK9810 <ruby45342@gamail.com>
2021-10-16 09:08:18 +0900

    level1 - 나머지1인수

---
+++ programmers실습/기타/level1/나머지1수.js
@@ -0,0 +1,11 @@
+function solution(n) {
+  for (let i = 1; i <= n; i++) {
+    if (n % i === 1) return i;
+  }
+}
+
+console.log(solution(10))
+console.log(solution(12))
+
+console.log(solution(10) === 3)
+console.log(solution(12) === 11)
