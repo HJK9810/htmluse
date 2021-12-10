91d123ce58f30e79c2ba34f8b655319e3821a7b3
HJK9810 <ruby45342@gamail.com>
2021-12-10 10:36:38 +0900

    트리탐색함수를 사용하여 체크

--- programmers실습/기타/위클리챌린지/9주차.js
+++ programmers실습/기타/위클리챌린지/9주차.js
@@ -1,3 +1,4 @@
+// 전력망 분리
 function solution(n, wires) {
   let first = [];
   let last = [];
@@ -6,44 +7,40 @@ function solution(n, wires) {
   // 트리 생성
   for (let i = 0; i < wires.length; i++) {
     const [nod1, nod2] = wires[i];
-    if(!tree[nod1]) tree[nod1] = [];
-    if(!tree[nod2]) tree[nod2] = [];
+    if (!tree[nod1]) tree[nod1] = [];
+    if (!tree[nod2]) tree[nod2] = [];
     tree[nod1].push(nod2);
     tree[nod2].push(nod1);
   }
 
-  let i = 1;
-  let j = n;
+  // 트리 탐색용 함수
+  const checkTree = (root, exc) => {
+    let count = 0;
+    const que = [root];
+    const visited = [];
+    visited[root] = tree;
 
-  while (first.length + last.length !== n) {
-    first.push(i);
-
-    if (tree[i].filter(el => el === i + 1).length !== -1) i += 1;
-    else {
-      for (let k = 1; i - k !== 0; k++) {
-        if (tree[i - k].filter(el => el === i + 1).length !== -1) {
-          i += 1;
-          break;
+    while (que.length) {
+      const cur = que.pop();
+      tree[cur].map(next => {
+        if (next !== exc && !visited[next]) {
+          visited[next] = tree;
+          que.push(next);
         }
-      }
+      });
+      count++;
     }
-
-    if (first.length + last.length === n) break;
-
-    last.push(j);
-
-    if (tree[j].filter(el => el === j - 1).length !== -1) j -= 1;
-    else {
-      for (let k = 1; j + k !== n; k++) {
-        if (tree[j + k].filter(el => el === j - 1).length !== -1) {
-          j -= 1;
-          break;
-        }
-      }
-    }
+    return count;
   }
-  console.log(`first: [${first}]\nlast:[${last}]`)
-  return Math.abs(first.length - last.length);
+
+  // 각 트리 경로를 탐색하며 수 체크
+  let answer = n;
+  wires.map(ary => {
+    const [a, b] = ary;
+    const check = Math.abs(checkTree(a, b) - checkTree(b, a));
+    answer = answer > check ? check : answer;
+  });
+  return answer;
 }
 
 console.log(solution(9, [[1, 3], [2, 3], [3, 4], [4, 5], [4, 6], [4, 7], [7, 8], [7, 9]]))
