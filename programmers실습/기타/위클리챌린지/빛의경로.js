b826afa61df332e09cc60a4951e83f9e1f26c994
HJK9810 <ruby45342@gamail.com>
2021-12-12 15:30:00 +0900

    level2 - 빛의경로사이클

---
+++ programmers실습/기타/위클리챌린지/빛의경로.js
@@ -0,0 +1,19 @@
+function solution(grid) {
+  let answer = [];
+  let root = []; // 빛의 이동경로를 체크하기 위한 배열
+  grid = grid.map(el => el.split(''));
+
+  grid.map(element => {
+    const leng = element.length;
+    const ary = Array.from({ length: leng }, () => [0, 0, 0, 0]);
+    root.push(ary);
+  });
+
+  return root;
+}
+
+console.log(solution(["SL", "LR"]))
+console.log(solution(["S"]))
+console.log(solution(["R", "R"]))
+
+console.log(`[16]\n[1,1,1,1]\n[4,4]`)
