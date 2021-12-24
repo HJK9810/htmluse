ce626fed2d28d93ef7523cbc59b38bc61e58916a
HJK9810 <ruby45342@gamail.com>
2021-12-24 09:06:18 +0900

    level2 - 게임 맴 최단거리

---
+++ programmers실습/기타/level2/게임맵최단.js
@@ -0,0 +1,17 @@
+function solution(maps) {
+  let answer = 0;
+  let now = [0, 0];
+  const BFS = () => {}
+
+  BFS();
+  return answer;
+}
+
+let ary1 = [[1,0,1,1,1],[1,0,1,0,1],[1,0,1,1,1],[1,1,1,0,1],[0,0,0,0,1]]
+let ary2 = [[1,0,1,1,1],[1,0,1,0,1],[1,0,1,1,1],[1,1,1,0,0],[0,0,0,0,1]]
+
+console.log(solution(ary1))
+console.log(solution(ary2))
+
+onsole.log(solution(ary1)===11)
+console.log(solution(ary2)===-1)
