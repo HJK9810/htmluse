24287b7be62ce64f900c7ad57236ee3c8fa5f663
HJK9810 <ruby45342@gamail.com>
2022-01-03 16:16:51 +0900

    bfs위한 while생성

--- programmers실습/기타/level2/게임맵최단.js
+++ programmers실습/기타/level2/게임맵최단.js
@@ -1,17 +1,23 @@
 function solution(maps) {
-  let answer = 0;
+  let answer = Number.MAX_SAFE_INTEGER;
   let now = [0, 0];
-  const BFS = () => {}
+  let end = [maps.length - 1, maps[0].length - 1]; // 맵의 좌표 : [세로 : n, 가로 : m]
+  let que = [], visited = [];
 
-  BFS();
+  while(visited.length) {
+    if(now[0]===end[0]&&now[1]===end[1]) {
+      break;
+    }
+  }
+  
   return answer;
 }
 
-let ary1 = [[1,0,1,1,1],[1,0,1,0,1],[1,0,1,1,1],[1,1,1,0,1],[0,0,0,0,1]]
-let ary2 = [[1,0,1,1,1],[1,0,1,0,1],[1,0,1,1,1],[1,1,1,0,0],[0,0,0,0,1]]
+let ary1 = [[1, 0, 1, 1, 1], [1, 0, 1, 0, 1], [1, 0, 1, 1, 1], [1, 1, 1, 0, 1], [0, 0, 0, 0, 1]]
+let ary2 = [[1, 0, 1, 1, 1], [1, 0, 1, 0, 1], [1, 0, 1, 1, 1], [1, 1, 1, 0, 0], [0, 0, 0, 0, 1]]
 
 console.log(solution(ary1))
 console.log(solution(ary2))
 
-onsole.log(solution(ary1)===11)
-console.log(solution(ary2)===-1)
+console.log(solution(ary1) === 11)
+console.log(solution(ary2) === -1)
